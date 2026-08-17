ad62ee510d80aa584672153b78cd3710f05efbd5
tekatjhb025 <tekatjhb025@student.wethinkcode.co.za>
2026-08-17 17:32:05 +0200

    Added coprimility tests

--- tests/test_co_prime.py
+++ tests/test_co_prime.py
@@ -4,3 +4,14 @@ class TestCoprime(unittest.TestCase):
     def test_coprime_numbers(self):
         self.assertTrue(is_coprime(5,26))
         
+    def test_non_coprime_numbers(self):
+        self.assertFalse(is_coprime(2,26))
+        
+    def test_another_coprime_pair(self):
+        self.assertTrue(is_coprime(7, 26))
+        
+    def test_another_non_coprime_pair(self):
+        self.assertFalse(is_coprime(13, 26))
+        
+if __name__ =="__main__":
+    unittest.main()
