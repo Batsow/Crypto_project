0ba28d8446211efb032380b929870c520d36db35
tekatjhb025 <tekatjhb025@student.wethinkcode.co.za>
2026-08-17 18:17:37 +0200

    Feat: Added Affine Cipher decryption and test

--- ciphers/affine_cipher.py
+++ ciphers/affine_cipher.py
@@ -23,3 +23,28 @@ def encrypt(text, a, b):
         ciphertext += ciphertext_letter
         
     return ciphertext
+
+
+'''
+Decrypt text using the Affine Cipher
+Formula: 
+        P = a⁻¹(C - b) mod 26
+'''
+
+def decrypt(text, a, b):
+    if not is_coprime(a, 26):
+        raise ValueError(f"{a} is not valif for the Affine Cipher. a = {a} must be coprime with 26")
+    
+    text = clean_text(text)
+    plaintext = ""
+    
+    inverse_a = modular_inverse(a, 26)
+    
+    for letter in text:
+        ciphertext_number = letter_to_number(letter)
+        plaintext_number = (inverse_a * (ciphertext_number - b)) % 26
+        
+        plaintext_letter = number_to_letter(plaintext_number)
+        plaintext += plaintext_letter
+        
+    return plaintext
--- tests/test_affine_cipher.py
+++ tests/test_affine_cipher.py
@@ -1,9 +1,22 @@
 import unittest
-from ciphers.affine_cipher import encrypt
+from ciphers.affine_cipher import encrypt, decrypt
 
 class TestAffineCipher(unittest.TestCase):
     def test_encrypt_single_letter(self):
         self.assertEqual(encrypt("H", 5, 8), "R")
+        
+    
+    def test_decrypt_single_letter(self):
+        self.assertEqual(decrypt("R", 5, 8), "H")
+        
+        
+    def test_encrypt_and_decrypt_word(self):
+        plaintext = "HELLO"
+        
+        ciphertext = encrypt(plaintext, 5, 8)
+        decrypted = decrypt(ciphertext, 5, 8)
+        
+        self.assertEqual(decrypted, plaintext)
         
         
 if __name__ == "__main__":
